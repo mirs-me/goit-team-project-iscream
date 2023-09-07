e7cd15cb530b73831f723ce81a1ef65de7da55ac
mirs-me <mir.svetoslavova@gmail.com>
2023-09-07 22:12:44 +0200

    js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './css/main.css';
-import './icons/icons.svg';
-import './images/desktop-logo@1x.png';
-import './images/desktop-logo@2x.png';
+import styles from './css/main.css';
+import styles from './icons/icons.svg';
+import styles from './images/desktop-logo@1x.png';
+import styles from './images/desktop-logo@2x.png';
