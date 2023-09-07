a382d40457f0cef69b68cd0480e9119dd406f92c
mirs-me <mir.svetoslavova@gmail.com>
2023-09-07 22:19:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '../src/css/main.css';
-import '../src/icons/icons.svg';
-import '../src/images/desktop-logo@1x.png';
-import '../src/images/desktop-logo@2x.png';
+import './css/main.css';
+import './icons/icons.svg';
+import './images/desktop-logo@1x.png';
+import './images/desktop-logo@2x.png';
