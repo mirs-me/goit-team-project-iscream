14f684db7ef8b7787ee48ace7a536a6b1fa5dfb8
mirs-me <mir.svetoslavova@gmail.com>
2023-09-10 21:47:20 +0200

    F-ing svg arrows

--- src/js/products.js
+++ src/js/products.js
@@ -14,6 +14,7 @@ productBtn.forEach(btn => {
             for (let i = 0; i < productsListItem.length; i++) {
               productsListItem[i].style.borderColor = '#aec6a5';
               productsListItemValue[i].style.backgroundColor = '#aec6a5';
+              productsListItemValue[i].style.boxShadow = '0px 6px 20px rgba(10, 136, 23, 0.25)'
             }
           }
        
@@ -24,6 +25,7 @@ productBtn.forEach(btn => {
             for (let i = 0; i < productsListItem.length; i++) {
               productsListItem[i].style.borderColor = '#efd478';
               productsListItemValue[i].style.backgroundColor = '#efd478';
+              productsListItemValue[i].style.boxShadow = `0px 6px 20px var(--yellow-underlay)`;
             }
           }
           if(e.target.id === 'ice-milkshake-btn') {
@@ -34,6 +36,8 @@ productBtn.forEach(btn => {
             for (let i = 0; i < productsListItem.length; i++) {
               productsListItem[i].style.borderColor = '#d6936d';
               productsListItemValue[i].style.backgroundColor = '#d6936d';
+              productsListItemValue[i].style.boxShadow = `0px 6px 20px var(--brown-underlay)`;
+            
             }
           }
           })});
