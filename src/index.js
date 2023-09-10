6df0176cb059419affde9406949611d2106053cc
Mrbabet <wieliczkokamil@gmail.com>
2023-09-10 14:21:55 +0200

    Added js to products-modal

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './css/main.css';
 import './js/mobile-menu.js';
 import './js/modal.js';
 import './js/customers.js';
+import './js/products.js'
 
--- src/js/products.js
+++ src/js/products.js
@@ -1,5 +1,39 @@
-const productBtn = document.querySelectorAll('.products-link')
-const productModal = document.querySelector('[data-modal=products]')
-const modalImage = productModal.querySelector('.product-modal-image')
-const productsListItem = document.querySelectorAll('.product-modal-stats-item')
-const productsListItemValue = document.querySelectorAll('.product-modal-stats-item-value')
+const productBtn = document.querySelectorAll('.products-link');
+const productModal = document.querySelector('[data-modal=products]');
+const modalImage = productModal.querySelector('.product-modal-image');
+const productsListItem = document.querySelectorAll('.product-modal-stats-item');
+const productsListItemValue = document.querySelectorAll('.product-modal-stats-item-value');
+
+productBtn.forEach(btn => {
+    btn.addEventListener('click', (e)=>{
+
+        if(e.target.id === 'ice-cream-btn') {
+            productModal.querySelector('.product-modal-title').textContent = 'Chocolate icecream';
+            modalImage.className = 'product-modal-image';
+      
+            for (let i = 0; i < productsListItem.length; i++) {
+              productsListItem[i].style.borderColor = '#aec6a5';
+              productsListItemValue[i].style.backgroundColor = '#aec6a5';
+            }
+          }
+       
+        if(e.target.id === 'ice-coffe-btn') {
+            productModal.querySelector('.product-modal-title').textContent = 'Ice Coffee';
+            modalImage.className = 'product-modal-image-coffee';
+      
+            for (let i = 0; i < productsListItem.length; i++) {
+              productsListItem[i].style.borderColor = '#efd478';
+              productsListItemValue[i].style.backgroundColor = '#efd478';
+            }
+          }
+          if(e.target.id === 'ice-milkshake-btn') {
+            productModal.querySelector('.product-modal-title').textContent = 'Milkshakes';
+            modalImage.className = 'product-modal-image-milkshakes';
+            console.log(productModal)
+      
+            for (let i = 0; i < productsListItem.length; i++) {
+              productsListItem[i].style.borderColor = '#d6936d';
+              productsListItemValue[i].style.backgroundColor = '#d6936d';
+            }
+          }
+          })});
